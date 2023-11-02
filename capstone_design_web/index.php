77a3954cd240c23cfe4e0cfc0b3fa06f0040209e
SPARK_ <78637281+SPARK031211@users.noreply.github.com>
2023-11-03 03:43:51 +0900

    Update index.php

--- capstone_design_web/index.php
+++ capstone_design_web/index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/db_board/main');
+	header('Location: '.$uri.'/castone_design_web/main');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
